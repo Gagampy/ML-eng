fb7333652a95079dde66ab1cc9e999d8374a1a74
Gagampy <muctepio@yandex.ru>
2020-09-01 13:07:47 +0300

    Lasso

--- 2-RTN/src/client/predict_by_model.py
+++ 2-RTN/src/client/predict_by_model.py
@@ -3,7 +3,7 @@ from logging import warning
 from typing import Union, Dict
 
 from pandas import DataFrame
-from sklearn.ensemble import GradientBoostingRegressor
+from sklearn.linear_model import Lasso
 from sklearn.metrics import mean_absolute_error
 from .utils import get_train_and_test
 import mlflow
@@ -11,7 +11,7 @@ import mlflow
 
 def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser()
-    parser.add_argument('-mn', default='GradientBoostingRegressor')
+    parser.add_argument('-mn', default='Lasso')
     parser.add_argument('-uri', default='http://rtn-mlflow-serv:5000')
     parser.add_argument('-en', default='rtn-title-len-regr')
     parser.add_argument('-m', default='mae_val')
@@ -55,7 +55,7 @@ if __name__ == '__main__':
     if run_df is not None:
         params = get_params_from_run_df(run_df, model_name=parser.mn, metric=parser.m)
 
-        model = GradientBoostingRegressor(**params)
+        model = Lasso(**params)
         model.fit(X=x_train, y=y_train)
         prediction = model.predict(X=x_test)
         print("MAE on test:", mean_absolute_error(y_test, prediction))
